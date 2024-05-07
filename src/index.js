26d7006c947bade66316b833ca87aff691662771
Daniel <daniel.marcos.1038@gmail.com>
2024-05-07 06:43:15 -0300

    sign up post and get, password encrypted with bcrypt

--- src/index.js
+++ src/index.js
@@ -1,34 +1,69 @@
 import express from "express"
 import cors from "cors"
+import dotenv from "dotenv"
+import { MongoClient } from "mongodb"
+import Joi from "joi"
+import bcrypt from "bcrypt"
 
+const newUserScheme = Joi.object({
+    name: Joi.string().min(3).max(20).required(),
+    image: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required()
+})
+
+dotenv.config()
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-const users = []
+const mongoClient = new MongoClient(process.env.MONGO_URI)
 
-app.post("/sign-up", (req, res) => {
-    const { username, image, email, password } = req.body
+try {
+    await mongoClient.connect()
+} catch (err) {
+    console.log(err)
+}
 
-    if (!username || !image || !email || !password) {
-        console.log("ERROR!! You wrote something wrong ")
-        return res.status(400).send({ message: "You wrote something wrong! Please confirm the data " })
+const db = mongoClient.db("users")
+const userCollection = db.collection("users")
+
+app.post("/sign-up", async (req, res) => {
+    const body = req.body
+    const { name, image, email, password } = req.body
+
+    try {
+        const isValid = await userCollection.findOne({ name })
+        if (isValid) {
+            return res.status(409).send({ message: "User already registered" })
+        }
+
+        const validation = newUserScheme.validate(body, { abortEarly: false })
+        if (validation.error) {
+            const errors = validation.error.details.map(error => error.message)
+            return res.status(422).send(errors)
+        }
+
+        const hashPassword = bcrypt.hashSync(password, 10)
+
+        await userCollection.insertOne({...body, password: hashPassword})
+        res.status(201)
+    } catch (err) {
+        console.log(err)
+        return res.status(409).send({ message: "ERROR" })
     }
 
-    const doesUserExist = users.find(user => user.username === username || user.email === email || user.image === image) 
-    if (doesUserExist) {
-        return res.status(409).send({message: "Conflict!! Try again !!"})
-    }
-
-    const newUser = { username, image, email, password }
-
-    users.push(newUser)
-    return res.send({message: "OK!"})
+    return res.send()
 })
 
-app.get("/sign-up", (req, res) => {
-    console.log("OK!!")
-    res.send(users)
+app.get("/sign-up", async (req, res) => {
+    try {
+        const participants = await userCollection.find().toArray()
+        res.send(participants)
+    } catch (err) {
+        console.log(err)
+        return res.status(400).send({message: "Error to get the participants"})
+    }
 
 })
 
